4fdd591dd70f10233f87a6bfbf7cf48ecd789d58
Upenda Reddy <53008656+upendrareddy0380@users.noreply.github.com>
2019-07-25 20:12:08 +0530

    update sample java file

--- myTestProject/src/main/java/myTestProject/Sample.java
+++ myTestProject/src/main/java/myTestProject/Sample.java
@@ -2,13 +2,16 @@ package myTestProject;
 
 public class Sample 
 {
-    public void add()
+    int a,b;
+	public void add()
     {
     	System.out.println(a+b);
     }
 	public static void main(String[] args) 
 	{
 		Sample s=new Sample();
+		s.a=5;
+		s.b=20;
 		s.add();
 
 	}
