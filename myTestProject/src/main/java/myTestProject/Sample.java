63ef1562d6b8d21f1f4d6f0f1736042f7862d0c0
batch299 <krishnamohanvenkat9@gmail.com>
2019-07-25 20:04:30 +0530

    1st time commit

---
+++ myTestProject/src/main/java/myTestProject/Sample.java
@@ -0,0 +1,16 @@
+package myTestProject;
+
+public class Sample 
+{
+    public void add()
+    {
+    	System.out.println(a+b);
+    }
+	public static void main(String[] args) 
+	{
+		Sample s=new Sample();
+		s.add();
+
+	}
+
+}
